ae27bf6feb58e925b074c4c126301b3a117f8cfd
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2018-03-28 14:17:00 +0000

    Day 2 Stream: adding s3auth and jwt verification
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
+import iopipe from '@iopipe/iopipe'
+import aws from 'aws-sdk'
+
+const IOpipe = iopipe();
+const S3 = new aws.S3();
+
+function encrypt () {
+}
+
+/* Endpoint that provides a JWT signed by us,
+   creating a valid key on S3 & allowing access */
+/* website will generate pub key, send to getKey
+   which will return a JWT for use in sending request */
+export const getRequestJwt = IOpipe((event, context, callback) => {
+  callback(null, jwt.sign(event.body, PRIVATE_KEY));
+});
+
 // eslint-disable-next-line import/prefer-default-export
-export const handler = (event, context, callback) => {
-  const p = new Promise((resolve) => {
-    resolve('success');
-  });
-  p
-    .then(() => callback(null, {
-      message: 'Go Serverless Webpack (Ecma Script) v1.0! First module!',
-      event,
-    }))
-    .catch(e => callback(e));
-};
+export const handler = IOpipe((event, context, callback) => {
+  var pathJwt = event.path;
+  jwt.verify(pathJwt, PRIVATE_KEY, (err, decodedJwt)) {
+    if err return callback("Error in pathJwt");
+ 
+    var encryptedRequest = encrypt(event, decodedJwt.aud);
+    const p = new Promise((resolve) => {
+      S3.putObject(
+        {
+          Bucket: process.env.S3BUCKET,
+          Key: decodedJwt.aud,
+          Body: encryptedRequest
+        },
+        () => { resolve() }
+      );
+    });
+    p
+      .then(() => callback(null, {
+        event,
+      }))
+      .catch(e => callback(e));
+  }
+});
---
+++ s3auth.js
@@ -0,0 +1,22 @@
+import iopipe from '@iopipe/iopipe'
+import jsonwebtoken as jwt from 'jsonwebtoken'
+import _ from 'lodash'
+
+const IOpipe = iopipe();
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+export const handler = IOpipe((event, context, callback) => {
+  console.log(event);
+  var uri = event[0].request.uri;
+  var authHeader = _.get(event[0].request.headers,
+    'authorization',
+    { [ { value: '' } ] }
+  )[0].value;
+
+  jwt.verify(authHeader, PRIVATE_KEY, {
+    audience: uri,
+  }, (err, decodedJwt) => {
+    if (err) return callback(err, { status: 401 });
+    callback(null, event);
+  })
+});
