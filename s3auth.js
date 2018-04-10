6be4beb8726aa75d13d98aab288a54e3599d28f7
Erica Windisch <73207+ewindisch@users.noreply.github.com>
2018-04-10 15:56:03 +0000

    Day 11: Authorization header for bearers
    Signed-off-by: Erica Windisch <73207+ewindisch@users.noreply.github.com>

--- s3auth.js
+++ s3auth.js
@@ -8,13 +8,17 @@ const IOpipe = iopipe();
 
 export const handler = IOpipe((event, context, callback) => {
   console.log(event);
-  var uri = event[0].request.uri;
-  var authHeader = _.get(event[0].request.headers,
+  var uri = event.Records[0].cf.request.uri;
+  var authHeader = _.get(event.Records[0].cf.request.headers,
     'authorization',
     [ { value: '' } ]
-  )[0].value;
+  )[0].value.split(" ");
 
-  jsonwebtoken.verify(authHeader, keys.public,
+  if (! authHeader[0].toLowerCase() === "bearer") {
+    return callback(null, { status: 401 });
+  }
+
+  jsonwebtoken.verify(authHeader[1], keys.public,
     (err, decodedJwt) => {
       if (err) return callback(err, { status: 401 });
       callback(null, event);
